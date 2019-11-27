4121f5a400ba41a48b1ff6c04a7a5d16f620250e
KodokuSeirei <kodokuseirei@gmail.com>
2019-11-27 17:19:55 +0400

    подправил имя проги

--- SinglyLinkedList/Properties/AssemblyInfo.cs
+++ SinglyLinkedList/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набор атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("WpfApp3")]
+[assembly: AssemblyTitle("SinglyLinkedList")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WpfApp3")]
+[assembly: AssemblyProduct("SinglyLinkedList")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
